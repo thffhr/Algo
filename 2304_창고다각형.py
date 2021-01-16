6f12412abbca9f9f16f2069ab92cb43673a862b8
thffhr <tedy55@naver.com>
2021-01-16 23:56:01 +0900

    210116

--- 2304_창고다각형.py
+++ 2304_창고다각형.py
@@ -1,35 +1,50 @@
+def FindMax(start, end, num):
+    # 채우기
+    for n in range(start, end):
+        W[n] = num
+    # 왼/오 탐색
+
 W = [0]*1000
-max_L = max_H = result = 0
+max_L = max_H = max_HIdx = 0
+min_L = 987654321
 N = int(input())
 for _ in range(N):
     L, H = map(int, input().split())
     W[L] = H
-    # max_L = max(W)
     if max_L < L:
         max_L = L
     if max_H < H:
         max_H = H
-        max_Hl = L
+        max_HIdx = L
+    if min_L > L:
+        min_L = L
 
-# 가장 높은 곳까지 창고 만들기
-lm = 0
-for i in range(max_Hl):
-    if lm < W[i]:
-        lm = W[i]
-    elif lm >= W[i]:
-        W[i] = lm
-# 나머지 만들기
-lm = 0
-for i in range(max_L, max_Hl, -1):
-    if lm < W[i]:
-        lm = W[i]
-    elif lm >= W[i]:
-        W[i] = lm
-# 더하기
-for i in range(max_L+1):
-    result += W[i]
+FindMax(max_HIdx, max_HIdx+1, max_H)
 
-print(result)
 
+
+
+
+
+
+
+# N = int(input())
+# Info = []
+# max_L = max_H = 0
+# for _ in range(N):
+#     Info.append(list(map(int, input().split(' '))))
+#     if max_L < Info[-1][0]:
+#         max_L = Info[-1][0]
+#     if max_H > Info[-1][1]:
+#         max_H = Info[-1][1]
+#
+# Map = [[0]*(max_L+1) for __ in range(max_H)]
+#
+# # 기둥 그리기
+# for n in range(N):
+#     for m in range(Info[n][1]):
+#         Map[m][Info[n][0]] = 1
+#
+# # 공간 채우기
 
 
