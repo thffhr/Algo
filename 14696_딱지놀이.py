674b749552436efa290a1ad82d7034ec4dfa762b
thffhr <tedy55@naver.com>
2021-01-24 23:57:27 +0900

    210124

---
+++ 14696_딱지놀이.py
@@ -0,0 +1,37 @@
+import copy
+R = int(input())
+A = []
+B = []
+for rr in range(2*R):
+    Info = list(map(int, input().split(' ')))
+    Input = [0, 0, 0, 0]
+    for i in range(Info[0]):
+        if Info[1:][i] == 1:
+            Input[0] += 1
+        elif Info[1:][i] == 2:
+            Input[1] += 1
+        elif Info[1:][i] == 3:
+            Input[2] += 1
+        else:
+            Input[3] += 1
+    if rr//2 or rr == 0:
+        A.append(copy.deepcopy(Input))
+    else:
+        B.append(copy.deepcopy(Input))
+
+Win = []
+for r in range(R):
+    winner = ''
+    for card in range(3, -1, -1):
+        if A[r][card] > B[r][card]:
+            winner = 'A'
+            break
+        elif A[r][card] < B[r][card]:
+            winner = 'B'
+            break
+    if winner == '':
+        winner = 'D'
+    Win.append(winner)
+
+for p in range(R):
+    print(Win[p])
