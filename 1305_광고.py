6a59f5538acbabb32333d6cff83076d53dd4c393
thffhr <tedy55@naver.com>
2021-01-27 23:58:43 +0900

    210127

--- 1305_광고.py
+++ 1305_광고.py
@@ -1,15 +1,8 @@
 L = int(input())
 ad = input()
-Ad = []
-for a in range(L):
-    Ad.append(ad[a])
-
-for l in range(1, L+1):
-    Ex = Ad[:l]
-    Test = []
-    while len(Test) < L:
-        for e in range(len(Ex)):
-            Test.append(Ex[e])
-    if Test == Ad:
-        print(l)
-        break
+idx = 0
+for i in range(1, L):
+    for j in range(i):
+        if ad[j] == ad[abs(j)-1]:
+            idx = j
+print(L-idx-1)
