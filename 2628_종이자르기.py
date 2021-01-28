af8e9f32bab1abb92dc238703870011b3b01b916
thffhr <tedy55@naver.com>
2021-01-29 00:17:55 +0900

    210128

--- 2628_종이자르기.py
+++ 2628_종이자르기.py
@@ -1,13 +1,3 @@
-# def PaperCut(WH, input_lst, output_lst):
-#     last_num = 0
-#     for i in range(len(input_lst)):
-#         if i:
-#             output_lst.append(input_lst[i] - output_lst[-1])
-#         else:
-#             output_lst.append(input_lst[i])
-#         last_num = input_lst[i]
-#     output_lst.append(WH - last_num)
-
 def maxNum(WH, cut_lst):
     last = 0
     max_len = 0
@@ -36,14 +26,4 @@ cut_H.sort()
 awsW = maxNum(W, cut_W)
 awsH = maxNum(H, cut_H)
 
-# W_list = []
-# H_list = []
-
-# PaperCut(W, cut_W, W_list)
-# PaperCut(H, cut_H, H_list)
-# aws = 0
-# for m in range(len(W_list)):
-#     for k in range(len(H_list)):
-#         if W_list[m] * H_list[k] > aws:
-#             aws = W_list[m] * H_list[k]
 print(awsW*awsH)
