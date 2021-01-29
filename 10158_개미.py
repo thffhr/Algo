b3691e86d2cc8890302cc54674138042e5889d2a
thffhr <tedy55@naver.com>
2021-01-30 00:06:47 +0900

    210129

--- 10158_개미.py
+++ 10158_개미.py
@@ -2,27 +2,15 @@ w, h = map(int, input().split(' '))
 p, q = map(int, input().split(' '))
 t = int(input())
 
-# 앞으로 갈 거리 p + t / q + t
-# w / h 로 나눈 값이 홀수일 경우 0부터 w / h 까지 증가
-# 아닐 경우 w / h 부터 0으로 감소
 if ((p + t) // w) % 2:
     x = w - ((p + t) % w)
 else:
     x = (p + t) % w
 
 if ((q + t) // h) % 2:
-    y = h - ((q + t) % w)
+    y = h - ((q + t) % h)
+
 else:
     y = (q + t) % h
 
 print(x, y)
-# p_plus = q_plus = 1
-# for t in range(T):
-#     if p + p_plus > w or p + p_plus < 0:
-#         p_plus *= -1
-#     if q + q_plus > h or q + q_plus < 0:
-#         q_plus *= -1
-#     p += p_plus
-#     q += q_plus
-#
-# print(p, q)
