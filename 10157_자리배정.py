c9a9c6f292c84cb4378729eeb655d27f478b9304
thffhr <tedy55@naver.com>
2021-02-01 23:26:36 +0900

    210201

--- 10157_자리배정.py
+++ 10157_자리배정.py
@@ -1,27 +1,21 @@
-def FindXY(x, y, n, aws):
-    if n == 0:
-        aws = str(x) + ' ' + str(y)
-        return
-    if aws == '':
-        Seat[y][x] = 1
-        Xx = [0, 1, 0, -1]
-        Yy = [1, 0, -1, 0]
-        for i in range(4):
-            new_x = x + Xx[i]
-            new_y = y + Yy[i]
-            if 0 <= new_x < C and 0 <= new_y < R and not Seat[new_y][new_x]:
-                FindXY(new_x, new_y, n-1, aws)
-    else:
-        return
-
 C, R = map(int, input().split(' '))
 K = int(input())
 Seat = [[0]*C for _ in range(R)]
-aws = ''
 
-FindXY(0, 0, K, aws)
-
-if aws == '':
+Xx = [0, 1, 0, -1]
+Yy = [1, 0, -1, 0]
+i = x = y = 0
+for k in range(K):
+    Seat[y][x] = 1
+    new_x = x + Xx[i % 4]
+    new_y = y + Yy[i % 4]
+    if 0 <= new_x < C and 0 <= new_y < R and not Seat[new_y][new_x]:
+        pass
+    else:
+        i += 1
+    x += Xx[i % 4]
+    y += Yy[i % 4]
+if K > C*R:
     print(0)
 else:
-    print(aws)
+    print(x - Xx[i % 4] + 1, y - Yy[i % 4] + 1)
