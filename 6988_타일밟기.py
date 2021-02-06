53c3a7a4d18ee723f0d5ec1757fddc2f71adf2c2
thffhr <tedy55@naver.com>
2021-02-06 23:50:52 +0900

    210206

--- 6988_타일밟기.py
+++ 6988_타일밟기.py
@@ -1,62 +1,76 @@
-def FindArr(i, j, n, depth):
-    if Check[i][j] == n:
-        lst.append(Tile[i])
-        for k in range(N):
-            if Check[j][k]:
-                FindArr(j, k, n, depth+1)
-                break
-    elif 0 <= j+1 < N:
-        FindArr(i, j+1, n, depth)
-    elif 0 <= i+1 < N:
-        FindArr(i+1, j, n, depth)
+def FindArr(i, j):
+    m = 0
+    arr = []
+    for t in range(j, N):
+        if Tile[j]+m == Tile[t]:
+            arr.append(Tile[t])
+            m += i
+    if len(arr) >= 3:
+        return arr
     else:
-        if depth >= 3: # 여기가 이상하다...
-            lst.append(Tile[i])
-        elif lst:
-            lst.pop()
-        return
+        return []
 
 
 N = int(input())
 Tile = list(map(int, input().split(' ')))
-Check = [[0]*N for _ in range(N)]
-
+max_sum = num_sum = 0
 for i in range(N):
     for j in range(N):
-        if i != j and Tile[j] - Tile[i] > 0:
-            Check[i][j] = Tile[j] - Tile[i]
-
-max_num = 0
-for n in range(1, N):
-    lst = []
-    FindArr(0, 0, n, 1)
-    num_sum = sum(lst)
-    if max_num < num_sum:
-        max_num = num_sum
-print(max_num)
-# for i in range(1, N):
-#     for j in range(N):
-#         if Tile[j] + i in Tile:
+        aws = FindArr(i, j)
+        if aws:
+            num_sum = sum(aws)
+        if max_sum < num_sum:
+            max_sum = num_sum
+print(max_sum)
 
 
-# max_sum = 0
-# for i in range(1, N):
-#     cnt = 0
-#     Step = []
-#     for j in range(1, N):
-#         if Tile[j] - Tile[j-1] == i:
-#             cnt += 1
-#             if Tile[j] not in Step:
-#                 Step.append(Tile[j])
-#             if Tile[j-1] not in Step:
-#                 Step.append(Tile[j-1])
-#         elif cnt < 3:
-#             cnt = 0
-#             Step = []
-#         elif cnt >= 3:
+# def FindArr(i, j, n, depth):
+#     for k in range(j, N):
+#         if Check[i][k] == n:
+#             lst.append(Tile[i])
+#             FindArr(k, k, n, depth+1)
 #             break
-#     num_sum = sum(Step)
-#     if max_sum < num_sum:
-#         max_sum = num_sum
+#     FindArr(i+1, i+1, n, depth)
+#     if depth >= 3:
+#         lst.append(Tile[i])
+#     if 1 <= len(lst) < 3:
+#         lst.pop()
+#     return
 #
-# print(max_sum)
+# # def FindArr(i, j, n, depth):
+# #     if Check[i][j] == n:
+# #         lst.append(Tile[i])
+# #         for k in range(N):
+# #             if Check[j][k]:
+# #                 FindArr(j, k, n, depth+1)
+# #                 break
+# #     elif 0 <= j+1 < N:
+# #         FindArr(i, j+1, n, depth)
+# #     elif 0 <= i+1 < N:
+# #         FindArr(i+1, j, n, depth)
+# #     else:
+# #         if depth >= 3: # 여기가 이상하다...
+# #             lst.append(Tile[i])
+# #         elif lst:
+# #             lst.pop()
+# #         return
+#
+#
+# N = int(input())
+# Tile = list(map(int, input().split(' ')))
+# Check = [[0]*N for _ in range(N)]
+#
+# for i in range(N):
+#     for j in range(N):
+#         if i != j and Tile[j] - Tile[i] > 0:
+#             Check[i][j] = Tile[j] - Tile[i]
+#
+# max_num = 0
+# for n in range(1, N):
+#     lst = []
+#     FindArr(0, 0, n, 1)
+#     num_sum = sum(lst)
+#     if max_num < num_sum:
+#         max_num = num_sum
+# print(max_num)
+#
