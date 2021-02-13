011dea88012ffd135a7e700228b5a9127b6fd862
thffhr <tedy55@naver.com>
2021-02-13 21:38:56 +0900

    210213

--- 13302_리조트.py
+++ 13302_리조트.py
@@ -1,37 +1,43 @@
-def BuyT(num, idx):
-    while num > 0:
-        Visited[idx] = 1
-        num -= 1
-        idx += 1
-
-def FindMin(idx, Price):
-    global N, Coupon, Min_P
-    if idx == N:
-        if Price < Min_P:
-            Min_P = Price
+def FindMin(idx, Visited, days, Price, Coupon):
+    global aws
+    if aws and Price > aws:
         return
-    if Price > Min_P:
-        return
-    for j in range(3):
-        if T[j] == 1 and Coupon >= 3:
-            Coupon -= 3
+    Visited[idx] = 1
+    if idx+1 in M_lst:
+        if days:
+            FindMin(idx+1, Visited, days - 1, Price, Coupon)
         else:
-            Price += P[j]
-        if T[j] == 3:
-            Coupon += 1
-        elif T[j] == 5:
-            Coupon += 2
-        BuyT(j, idx)
+            FindMin(idx + 1, Visited, days, Price, Coupon)
+    else:
+        if days:
+            FindMin(idx+1, Visited, days - 1, Price, Coupon)
+        else:
+            for i in range(3):
+                if idx + 1 < N:
+                    if i == 0:
+                        FindMin(idx + 1, Visited, days + T[i] - 1, Price + P[i], Coupon)
+                        if Coupon >= 3:
+                            FindMin(idx + 1, Visited, days + T[i] - 1, Price, Coupon - 3)
+                    else:
+                        if i == 1:
+                            Coupon += 1
+                        elif i == 2:
+                            Coupon += 2
+                        FindMin(idx+1, Visited, days+T[i]-1, Price+P[i], Coupon)
+    if idx >= N - 1  and aws > Price:
+        aws = Price
+    if Visited[idx]:
+        Visited[idx] = 0
+    return
 
 
 N, M = map(int, input().split(' '))
 M_lst = list(map(int, input().split(' ')))
+Visited = [0]*N
 T = [1, 3, 5]
 P = [10000, 25000, 37000]
-Visited = [0]*N
-Coupon = Price = 0
-Min_P = 987654321
+aws = 987654321
 
-FindMin(0, 0)
+FindMin(0, Visited, 0, 0, 0)
 
-print(Min_P)
+print(aws)
