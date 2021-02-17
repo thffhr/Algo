6b2661ef57ccec6d37040090e7f2f88be261075b
thffhr <tedy55@naver.com>
2021-02-17 23:58:55 +0900

    210217

--- 1799_비숍.py
+++ 1799_비숍.py
@@ -1,25 +1,31 @@
-def Set(x, y, cnt):
-    if 0 <= x < N and 0 <= y < N:
-        for a in range(y):
-            for b in range(x):
-                Map[a][b] = 0
-        for c in range(y, N):
-            for d in range(x, N):
-                Map[c][d] = 0
-        for e in range(y, N):
-            for f in range(x):
-                if Map[e][f] < 0:
-                    return
-        for g in range(y):
-            for h in range(x, N):
-                if Map[g][h] < 0:
-                    return
-        Map[y][x] = -1
-        Set()
+def Set(x, y, cnt, tf):
+    if tf:
+
+    else:
+        pass
+
+
 
 N = int(input())
 Map = []
 max_cnt = 0
-for i in range(N):
+for _ in range(N):
     Map.append(list(map(int, input().split(' '))))
 
+Black = []
+White = []
+
+for i in range(N):
+    for j in range(N):
+        if Map[i][j]:
+            if (i + j)%2:
+                White.append([i, j])
+            else:
+                Black.append([i, j])
+        Map[i][j] = 0
+
+for w in range(len(White)):
+    Set(White[w][1], White[w][0], 0, True)
+
+for b in range(len(Black)):
+    Set(Black[b][1], White[w][0], 0, False)
